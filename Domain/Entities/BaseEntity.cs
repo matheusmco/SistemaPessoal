fe007ab5416e0ec555bf8e1f08f3f422fe286cb6
Matheus Oliveira <matheus0mco@gmail.com>
2019-06-04 19:27:41 -0300

    feat:adding base methods

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -1,12 +1,39 @@
+using System.Collections.Generic;
+
 namespace SistemaPessoal.Domain.Entities
 {
-    abstract class BaseEntity
+    public abstract class BaseEntity
     {
         protected virtual int Id { get; set; }
 
         public BaseEntity(int id)
         {
             Id = id;
+        }
+
+        public virtual int Save(BaseEntity Entidade)
+        {
+            return default(int);
+        }
+
+        public virtual int Update(BaseEntity Entidade)
+        {
+            return default(int);
+        }
+
+        public virtual BaseEntity GetById(int id)
+        {
+            return default(BaseEntity);
+        }
+
+        public virtual IEnumerable<BaseEntity> GetAll()
+        {
+            return null;
+        }
+
+        public virtual void Delete(int id)
+        {
+            
         }
     }
 }
