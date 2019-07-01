7ec33618f87c3155811f8dfa29d93a32e62f3bc1
Matheus Oliveira <matheus0mco@gmail.com>
2019-07-01 19:32:40 -0300

    feat:adding swagger

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace SistemaPessoal
 {
@@ -26,6 +22,31 @@ namespace SistemaPessoal
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Personal System",
+                        Version = "v1",
+                        Description = "Sistema Pessoal",
+                        Contact = new Contact
+                        {
+                            Name = "Matheus Oliveira",
+                            Url = "https://github.com/matheusmco"
+                        }
+                    });
+
+                var applicationBasePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var applicationName = PlatformServices.Default.Application.ApplicationName;
+                var xmlDocumentPath = Path.Combine(applicationBasePath, $"{applicationName}.xml");
+
+                if (File.Exists(xmlDocumentPath))
+                {
+                    options.IncludeXmlComments(xmlDocumentPath);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +61,17 @@ namespace SistemaPessoal
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "docs/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "docs";
+                c.SwaggerEndpoint("./v1/swagger.json", "Sistema Pessoal");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
